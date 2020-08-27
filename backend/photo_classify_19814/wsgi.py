090f72637ea87226d178859d33eebf57ab5af124
Crowdbotics <team@crowdbotics.com>
2020-08-27 11:27:52 +0000

    Update models

--- backend/photo_classify_19814/wsgi.py
+++ backend/photo_classify_19814/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'photo_classify_19814.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "photo_classify_19814.settings")
 
 application = get_wsgi_application()
